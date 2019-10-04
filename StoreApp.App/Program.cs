5fa15f91acaa509eaddb9f4f4c4815bb058af8f9
PhatNguyen203 <Lee0ne203@gmail.com>
2019-10-04 14:19:47 -0500

    Welcome Page

--- StoreApp.App/Program.cs
+++ StoreApp.App/Program.cs
@@ -6,7 +6,7 @@ namespace StoreApp.App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ StoreApp.App/Welcome.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace StoreApp.App
+{
+    public class Welcome
+    {
+      public void welcomePage
+      {
+          
+      } 
+    }
+}
